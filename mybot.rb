4c807017a4d593f1f4c72e71466aef6cd9763969
Jane Threefoot <jane.threefoot@gmail.com>
2019-04-17 22:37:46 -0400

    added cheer command for cheercrab

--- mybot.rb
+++ mybot.rb
@@ -5,10 +5,14 @@ cfg_filename = "config.json"
 cfg_file = open(cfg_filename).read
 config = JSON.parse(cfg_file)
 
-bot = Discordrb::Bot.new token: config["token"], client_id: config["client_id"]
+bot = Discordrb::Commands::CommandBot.new token: config["token"], client_id: config["client_id"], prefix: '!'
 
 bot.message(with_text: 'Ping!') do |event|
 	event.respond 'Pong!'
+end
+
+bot.command(:cheer) do |event|
+	event.channel.send_file File.open('images/cheerleader_crab.gif', 'r')
 end
 
 bot.run
